f370da19ca373de63b7a0b51d9bf283eda5681b2
ifyboy001 <anihifeanyichukwu@gmail.com>
2022-08-28 17:36:19 +0100

    comment file in C

---
+++ comment_in_c.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+//this file explains the code used in commenting in C programming language
+
+//FOR SINGLE LINE COMMENT:
+//Two forward slashes are used i.e '//' ate the beginning of the line
+
+//FOR MULTIPLE LINE COMMENT
+//A forward  and a star slash is used at the start of the comment and same at the end of the comment i.e
+/*A
+multi-line
+comment */
+// USES OF COMMENT IN C PROGRAMMING
+/* 1. for explaining some bunch for code or a particular line of code and what it is expected of the outcome
+      which will help in future for correction or editing or enhancing
+   2. for commenting out some codes you don't want the computer to execute at the moment instead of deleting the code
+      which you probably gonna add in the future
+   3. explanation
+*/
